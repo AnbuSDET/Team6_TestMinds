eb525c6eddb8a877377e24a306e0907270ac145e
Vidhya Manickaraj <vidhyamanickaraj@Vidhyas-Air.home>
2025-02-23 21:10:59 -0600

    Program features

--- src/test/java/commonUtilities/Context.java
+++ src/test/java/commonUtilities/Context.java
@@ -4,6 +4,7 @@ import driverManager.DriverFactory;
 import pageObjects.ClassPage;
 import pageObjects.ClassPage_Part2;
 import pageObjects.LoginPage;
+import pageObjects.ProgramPage_Part2;
 
 public class Context {
 
@@ -11,12 +12,14 @@ public class Context {
 	private LoginPage loginPage;
 	private ClassPage classPage;
 	private ClassPage_Part2 classpage2;
+	private ProgramPage_Part2 programpage2;
 
 	public Context() {
 		driverFactory = DriverFactory.getInstance();
 		loginPage = new LoginPage(driverFactory.setupDriver());
 		classPage = new ClassPage(driverFactory.setupDriver());
 		classpage2 = new ClassPage_Part2(driverFactory.setupDriver());
+		programpage2=new ProgramPage_Part2(driverFactory.setupDriver());
 
 	}
 
@@ -35,4 +38,9 @@ public class Context {
 	public ClassPage_Part2 getClassPage2() {
 		return classpage2;
 	}
+	
+	public ProgramPage_Part2 getProgramPage2() {
+		return programpage2;
+	}
+
 }
---
+++ src/test/resources/Features/03_04_ProgramSearch.feature
@@ -0,0 +1,21 @@
+@programsearch
+Feature: Search Program
+
+ Background: 
+    Given Admin is in login Page.
+    When Admin enter valid data with "Login" and "Login with valid credentials" in all fields and clicks login button
+    Then Admin should land on Home page
+    Then Admin is on the Manage Program page after login
+    
+Scenario Outline: Verify searching added program
+ Then Admin searches with newly created program name from "<sheetName>" with scenario name "<scenarioName>"
+ 
+Examples:
+
+ |sheetName|scenarioName|
+ |Program|Searching the added program using program name|
+ |Program|Searching the added program using program description|
+ #|Program|Searching the added program using invalid program name|
+ #|Program|Searching the added program using partial program name|
+ 
+    
